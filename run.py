ff66feb2d16669ce8c640dbb7d2d224d27aa7778
Anna Maria Salamon <annamariapix@hotmail.com>
2024-03-23 22:59:25 +0000

    Collect sales data for  last 5 markets in lists of colums

--- run.py
+++ run.py
@@ -111,6 +111,22 @@ def update_surplus_worksheet(data):
     """  
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
